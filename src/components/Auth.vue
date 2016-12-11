237e4738888294f1748ec6f36573d8097caf2056
wulei123 <1534563365@qq.com>
2016-12-11 17:20:23 +0800

    update a struct

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import {appurl} from '../config.js';
+import {APIURL,APPURL,APPID,CALLBACK} from '../config.js';
 export default {
     name: 'auth',
   data () {
@@ -12,23 +12,20 @@ export default {
     }
   },
   mounted(){
-this.$http.options.xhr = { withCredentials: true };
     this.vq = this.$route.query.verify_request;
-    let request_url = appurl+'/ballot/auth?vq='+this.vq;
-    console.log(request_url);
+    console.log(this.$route.fullPath);
     let verify_request = this.$route.query.verify_request;
     if(!sessionStorage.getItem('islogin')){
       if(!verify_request){
-        console.log('has not verify_request')
-        window.location="https://openapi.yiban.cn/oauth/authorize?client_id=484b6cb2a841acbd&redirect_uri=http://f.yiban.cn/iapp54363&display=html";
+        window.location="https://openapi.yiban.cn/oauth/authorize?client_id="+APPID+"&redirect_uri="+CALLBACK+"&display=html";
       }else{
-      	this.$http.get(request_url).then((response)=>{
+      	this.$http.get(APIURL+'/ballot/auth?vq='+verify_request).then((response)=>{
       if(response.data==1){
         sessionStorage.setItem('islogin', true);
+        window.location=APPURL;
       }else{
         sessionStorage.setItem('islogin', false);
       }
-      console.log(sessionStorage.getItem('islogin'));
     })
       }
     }
--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
-let appurl='http://localhost:8086';
+let APIURL='http://localhost:8086';
+let APPURL='http://localhost:8080';
+let APPID='484b6cb2a841acbd';
+let CALLBACK='http://f.yiban.cn/iapp54363';
 
-
-
-export {appurl}
+export {APIURL,APPURL,APPID,CALLBACK}
